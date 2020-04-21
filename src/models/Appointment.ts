bc1a69255db70dc7b8575fe0fbce044094bbee8a
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2020-04-21 15:48:28 -0300

    Relacionamento nos models

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -4,15 +4,33 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+import User from './User';
 
+/**
+ * Principio KISS - Keep It Simple & Stupid - mantenha simples seus código
+ */
 @Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  /**
+   * Tipos de Relacionamentos
+   * Um para Um (OneToOne)
+   * Um para Muitos (OneToMany)
+   * Muitos para Muitos(ManyToMany)
+   */
+  @ManyToOne(() => User)
+  @JoinColumn({
+    name: 'provider_id',
+  })
+  provider: User;
 
   @Column('time without time zone')
   date: Date;
